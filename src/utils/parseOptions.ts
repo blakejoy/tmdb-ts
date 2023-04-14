1200064194bdbf82642923044ec8e8dae1473b02
DerPenz <mike2407@t-online.de>
2023-04-14 15:54:27 +0200

    moved option parsing in superclass 'get' method

--- src/utils/parseOptions.ts
+++ src/utils/parseOptions.ts
@@ -1,5 +1,5 @@
 export function parseOptions(
-  options?: { [s: string]: any },
+  options?: Record<string, any>,
 ): string {
   return options
     ? new URLSearchParams(Object.entries(options)).toString()
