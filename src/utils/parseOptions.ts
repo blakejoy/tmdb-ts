eaa5f9a751d3d81f91fdaceba0c85f5ef31c46a2
Fadhil Ahmad <fadzbobby95@gmail.com>
2024-10-02 12:36:51 +0800

    fix: images is empty because languages undefined on querystring

--- src/utils/parseOptions.ts
+++ src/utils/parseOptions.ts
@@ -1,4 +1,9 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 export function parseOptions(options?: Record<string, any>): string {
-  return options ? new URLSearchParams(Object.entries(options)).toString() : '';
+  return options
+    ? new URLSearchParams(
+        Object.entries(options)
+          .filter(([k, v]) => v) // remove undefined
+      ).toString()
+    : '';
 }
