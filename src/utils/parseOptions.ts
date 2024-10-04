9e1e65205ca910fcf628ff03cdc1a34115939c19
Blake Joynes <blakejoynes@gmail.com>
2024-10-04 01:58:25 -0400

    fix linting

--- src/utils/parseOptions.ts
+++ src/utils/parseOptions.ts
@@ -2,8 +2,7 @@
 export function parseOptions(options?: Record<string, any>): string {
   return options
     ? new URLSearchParams(
-        Object.entries(options)
-          .filter(([k, v]) => v) // remove undefined
+        Object.entries(options).filter(([, v]) => v) // remove undefined
       ).toString()
     : '';
 }
